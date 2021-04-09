ca4e64715390616c283b6d5b98c726676360407c
ruzia <ruzia@idealab.jp>
2021-04-09 17:59:42 +0900

    依存 gem を require するように

--- lib/sg_lambda_rails.rb
+++ lib/sg_lambda_rails.rb
@@ -2,7 +2,10 @@
 
 require_relative "sg_lambda_rails/version"
 
+require 'aws-sdk-ssm'
+require 'lambdakiq'
 require 'activerecord-refresh_connection'
+require 'dotenv/rails-now'
 
 module SgLambdaRails
   class Error < StandardError; end
