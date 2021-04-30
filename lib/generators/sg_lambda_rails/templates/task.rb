4f1f831615c31758c83c0a523d13aeaa69bb4674
ruzia <ruzia@idealab.jp>
2021-04-30 15:02:56 +0900

    task は環境変数を簡単に渡したりできるようにするために単純なコマンドの実行に変更

--- lib/generators/sg_lambda_rails/templates/task.rb
+++ lib/generators/sg_lambda_rails/templates/task.rb
@@ -1,22 +1,7 @@
-require_relative 'config/boot'
-require 'dotenv' ; Dotenv.load ".env.#{ENV['RAILS_ENV']}"
-require_relative 'config/application'
-require_relative 'config/environment'
-
-Rails.application.load_tasks
-
 def handler(event:, context:)
   event_task = event['task']
   puts "Invoke task lambda with '#{event}'"
   puts "target_task: #{event_task}"
 
-  if (m = event_task.match(/(.+)\[(.*)\]/))
-    task = m[1]
-    args = m[2].split(',').map(&:strip)
-    Rake::Task[task].invoke(*args)
-    Rake::Task[task].reenable
-  else
-    Rake::Task[event_task].invoke
-    Rake::Task[event_task].reenable
-  end
+  puts `bin/rails #{event_task}`
 end
