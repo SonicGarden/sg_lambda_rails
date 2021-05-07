59f23aa372bd7bcae5e52b84a6535ceee8e825b3
ruzia <ruzia@idealab.jp>
2021-05-08 05:06:22 +0900

    cloudfront 用の middleware を追加

---
+++ lib/sg_lambda_rails/cloud_front_header.rb
@@ -0,0 +1,16 @@
+module SgLambdaRails
+  class CloudFrontHeader
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      cloudfront_forwarded_proto = env['HTTP_CLOUDFRONT_FORWARDED_PROTO']
+      if cloudfront_forwarded_proto && cloudfront_forwarded_proto.length > 0
+        env['HTTP_X_FORWARDED_PROTO'] = cloudfront_forwarded_proto
+      end
+
+      @app.call(env)
+    end
+  end
+end
--- lib/sg_lambda_rails/rails.rb
+++ lib/sg_lambda_rails/rails.rb
@@ -1,11 +1,11 @@
 require 'activerecord-refresh_connection'
+require 'sg_lambda_rails/cloud_front_header'
 
 module SgLambdaRails
   class Railtie < Rails::Railtie
     initializer "sg_lambda_rails.configure_rails_initialization" do |app|
-      # Insert activerecord-refresh_connection middleware.
-      app.config.middleware.insert_before ActionDispatch::Executor,
-        ActiveRecord::ConnectionAdapters::RefreshConnectionManagement
+      app.config.middleware.insert_before ActionDispatch::Executor, ActiveRecord::ConnectionAdapters::RefreshConnectionManagement
+      app.config.middleware.insert_before ActiveRecord::ConnectionAdapters::RefreshConnectionManagement, SgLambdaRails::CloudFrontHeader
     end
   end
 end
