5dc1eec0468de15be144bd396c359ca4d884c6e0
ruzia <ruzia@idealab.jp>
2021-05-14 18:06:31 +0900

    CloudFront でカスタムドメイン設定をなくすと origin のチェックに失敗して 403 になる問題に対処

--- lib/sg_lambda_rails/rails.rb
+++ lib/sg_lambda_rails/rails.rb
@@ -8,7 +8,7 @@ module SgLambdaRails
       app.config.middleware.insert_before ActionDispatch::Executor, ActiveRecord::ConnectionAdapters::RefreshConnectionManagement
 
       # CloudFront を利用した際に origin のチェックが通らないのを回避するためのワークアラウンド
-      # app.config.action_controller.forgery_protection_origin_check = false
+      app.config.action_controller.forgery_protection_origin_check = false
     end
   end
 end
