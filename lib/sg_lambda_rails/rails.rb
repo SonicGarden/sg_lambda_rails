99fb37a5d156bd6d89b51872e376a3665103be91
ruzia <ruzia@idealab.jp>
2021-05-15 12:31:55 +0900

    実験のために一時的にコメントアウト

--- lib/sg_lambda_rails/rails.rb
+++ lib/sg_lambda_rails/rails.rb
@@ -6,11 +6,11 @@ module SgLambdaRails
   class Railtie < Rails::Railtie
     initializer "sg_lambda_rails.configure_rails_initialization" do |app|
       app.config.middleware.insert 0, SgLambdaRails::AdjustCloudFrontHeaders
-      app.config.middleware.insert_after SgLambdaRails::AdjustCloudFrontHeaders, SgLambdaRails::AdjustHostHeader
+      # app.config.middleware.insert_after SgLambdaRails::AdjustCloudFrontHeaders, SgLambdaRails::AdjustHostHeader
       app.config.middleware.insert_before ActionDispatch::Executor, ActiveRecord::ConnectionAdapters::RefreshConnectionManagement
 
       # CloudFront を利用した際に origin のチェックが通らないのを回避するためのワークアラウンド
-      app.config.action_controller.forgery_protection_origin_check = false
+      # app.config.action_controller.forgery_protection_origin_check = false
     end
   end
 end
